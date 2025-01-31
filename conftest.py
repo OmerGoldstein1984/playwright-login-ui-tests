f1b30145b81c88cb2a4198f870839d87b7aca351
Omer Goldstein <omergo@payoneer.com>
2025-01-31 10:34:57 +0200

    select products and checkout

--- conftest.py
+++ conftest.py
@@ -8,9 +8,10 @@ def browser():
     with sync_playwright() as p:
         browser = p.chromium.launch(
             executable_path="C:\\Users\\omergo\\Downloads\\chromium\\chrome-win\\chrome.exe",
-            headless=False
-
+            headless=False,
+            args=["--start-maximized"]
         )
-        yield browser
-
+        context = browser.new_context(no_viewport=True)
+        page=context.new_page()
+        yield page
         browser.close()
--- test_page.py
+++ test_page.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 from playwright.async_api import async_playwright, expect
 
 def test_example(browser):
@@ -23,18 +25,17 @@ def test_login_rahulshuttyacademy(browser):
 
 def test_selectAndCheckout(browser):
     products=['iphone X','Blackberry']
-    page =  browser.new_page()
-    page.evaluate("() => { window.moveTo(0, 0); window.resizeTo(screen.width, screen.height); }")
-    page.goto('https://rahulshettyacademy.com/loginpagePractise/')
-    page.get_by_label('username:').fill('rahulshettyacademy')
-    page.get_by_label('password:').fill('learning')
-    page.get_by_role('combobox').select_option('teach')
-    page.get_by_role('link', name="terms and conditions").click()
-    page.get_by_role('button', name='Sign In').click()
+    browser.goto('https://rahulshettyacademy.com/loginpagePractise/')
+    browser.get_by_label('username:').fill('rahulshettyacademy')
+    browser.get_by_label('password:').fill('learning')
+    browser.get_by_role('combobox').select_option('teach')
+    browser.get_by_role('link', name="terms and conditions").click()
+    browser.get_by_role('button', name='Sign In').click()
     for product in products:
-       prodductContainer= page.get_by_role('app-card').filter(has_text=product)
-       prodductContainer.get_by_role('button').click()
-
-    checkoutBtn= page.get_by_role('button').filter(has_text='Checkout')
+       prodductContainer= browser.locator('.card').filter(has_text=product)
+       btn = prodductContainer.get_by_role('button')
+       btn.click()
+    checkoutBtn= browser.locator('.btn').filter(has_text=' Checkout ( 2 ) ')
     checkoutBtn.click()
+    sleep(5)
 
